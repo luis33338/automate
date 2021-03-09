bd78bfeea59a45ef3e05399d884d80df899df620
Chef Expeditor <chef-ci@chef.io>
2021-03-09 18:57:40 +0000

    Bump Habitat version
    This pull request was triggered automatically via Expeditor.
    
    This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required.

--- .expeditor/create-manifest.rb
+++ .expeditor/create-manifest.rb
@@ -308,9 +308,9 @@ unless no_pin_hab
   # components/automate-deployment/habitat/plan.sh
   #
   # WARNING: These pins are managed by .expeditor/update_habitat.sh.
-  pins["hab"]          = { "origin" => "core", "name" => "hab",          "version" => "1.6.181", "release" => "20201030172917"}
-  pins["hab-sup"]      = { "origin" => "core", "name" => "hab-sup",      "version" => "1.6.181", "release" => "20201030181202"}
-  pins["hab-launcher"] = { "origin" => "core", "name" => "hab-launcher", "version" => "14772",  "release" => "20201030181158"}
+  pins["hab"]          = { "origin" => "core", "name" => "hab",          "version" => "1.6.267", "release" => "20210304183447"}
+  pins["hab-sup"]      = { "origin" => "core", "name" => "hab-sup",      "version" => "1.6.267", "release" => "20210304191502"}
+  pins["hab-launcher"] = { "origin" => "core", "name" => "hab-launcher", "version" => "15249",  "release" => "20210304191504"}
 end
 
 # CONFIGURATION
