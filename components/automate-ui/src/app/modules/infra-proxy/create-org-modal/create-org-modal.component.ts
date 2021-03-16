bc924171a47baaeef4e8696cc689dd081ecb295a
seajamied <jdegnan@chef.io>
2021-03-16 16:43:16 -0700

    Allow first focus to be a chef-button
    Added logic to allow a developer to have chef-button component
    be a first focused item, this is necessary because the chef-button
    uses a nested button.  I also added the first focus attribute to
    the infra > chef servers > Add Chef Organization modal as proof of concept
    
    Signed-off-by: seajamied <jdegnan@chef.io>

--- components/automate-ui/src/app/modules/infra-proxy/create-org-modal/create-org-modal.component.ts
+++ components/automate-ui/src/app/modules/infra-proxy/create-org-modal/create-org-modal.component.ts
@@ -68,6 +68,6 @@ export class CreateOrgModalComponent implements OnInit, OnChanges {
   }
 
   private isNavigationKey(event: KeyboardEvent): boolean {
-    return event.key === 'Shift' || event.key === 'Tab';
+    return event.key === 'Shift' || event.key === 'Tab' || event.key === 'Enter';
   }
 }
