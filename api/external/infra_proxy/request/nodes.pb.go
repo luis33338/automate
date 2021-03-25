rheehot/automate-1

API to update node attributes
Signed-off-by: Vivek Singh <vivek.singh@msystechnologies.com>

api/external/infra_proxy/request/nodes.pb.go
@@ -437,6 +437,90 @@ func (x *UpdateNode) GetOverrideAttributes() *_struct.Struct {
 	return nil
 }
 
+type UpdateNodeAttributes struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Chef organization ID.
+	OrgId string `protobuf:"bytes,1,opt,name=org_id,json=orgId,proto3" json:"org_id,omitempty"`
+	// Chef Infra Server ID.
+	ServerId string `protobuf:"bytes,2,opt,name=server_id,json=serverId,proto3" json:"server_id,omitempty"`
+	// Node name.
+	Name string `protobuf:"bytes,3,opt,name=name,proto3" json:"name,omitempty"`
+	// Node attribute type. (e.g. 'normal', 'default').
+	Type string `protobuf:"bytes,4,opt,name=type,proto3" json:"type,omitempty"`
+	// Node attributes JSON.
+	Attributes *_struct.Struct `protobuf:"bytes,5,opt,name=attributes,proto3" json:"attributes,omitempty"`
+}
+
+func (x *UpdateNodeAttributes) Reset() {
+	*x = UpdateNodeAttributes{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_external_infra_proxy_request_nodes_proto_msgTypes[5]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *UpdateNodeAttributes) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UpdateNodeAttributes) ProtoMessage() {}
+
+func (x *UpdateNodeAttributes) ProtoReflect() protoreflect.Message {
+	mi := &file_external_infra_proxy_request_nodes_proto_msgTypes[5]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UpdateNodeAttributes.ProtoReflect.Descriptor instead.
+func (*UpdateNodeAttributes) Descriptor() ([]byte, []int) {
+	return file_external_infra_proxy_request_nodes_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *UpdateNodeAttributes) GetOrgId() string {
+	if x != nil {
+		return x.OrgId
+	}
+	return ""
+}
+
+func (x *UpdateNodeAttributes) GetServerId() string {
+	if x != nil {
+		return x.ServerId
+	}
+	return ""
+}
+
+func (x *UpdateNodeAttributes) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+func (x *UpdateNodeAttributes) GetType() string {
+	if x != nil {
+		return x.Type
+	}
+	return ""
+}
+
+func (x *UpdateNodeAttributes) GetAttributes() *_struct.Struct {
+	if x != nil {
+		return x.Attributes
+	}
+	return nil
+}
+
 var File_external_infra_proxy_request_nodes_proto protoreflect.FileDescriptor
 
 var file_external_infra_proxy_request_nodes_proto_rawDesc = []byte{
@@ -501,12 +585,23 @@ var file_external_infra_proxy_request_nodes_proto_rawDesc = []byte{
 	0x65, 0x5f, 0x61, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65, 0x73, 0x18, 0x0b, 0x20, 0x01,
 	0x28, 0x0b, 0x32, 0x17, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74,
 	0x6f, 0x62, 0x75, 0x66, 0x2e, 0x53, 0x74, 0x72, 0x75, 0x63, 0x74, 0x52, 0x12, 0x6f, 0x76, 0x65,
-	0x72, 0x72, 0x69, 0x64, 0x65, 0x41, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65, 0x73, 0x42,
-	0x3b, 0x5a, 0x39, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x68,
-	0x65, 0x66, 0x2f, 0x61, 0x75, 0x74, 0x6f, 0x6d, 0x61, 0x74, 0x65, 0x2f, 0x61, 0x70, 0x69, 0x2f,
-	0x65, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x69, 0x6e, 0x66, 0x72, 0x61, 0x5f, 0x70,
-	0x72, 0x6f, 0x78, 0x79, 0x2f, 0x72, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x72, 0x72, 0x69, 0x64, 0x65, 0x41, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65, 0x73, 0x22,
+	0xab, 0x01, 0x0a, 0x14, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x4e, 0x6f, 0x64, 0x65, 0x41, 0x74,
+	0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65, 0x73, 0x12, 0x15, 0x0a, 0x06, 0x6f, 0x72, 0x67, 0x5f,
+	0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x6f, 0x72, 0x67, 0x49, 0x64, 0x12,
+	0x1b, 0x0a, 0x09, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x5f, 0x69, 0x64, 0x18, 0x02, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x08, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x49, 0x64, 0x12, 0x12, 0x0a, 0x04,
+	0x6e, 0x61, 0x6d, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65,
+	0x12, 0x12, 0x0a, 0x04, 0x74, 0x79, 0x70, 0x65, 0x18, 0x04, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04,
+	0x74, 0x79, 0x70, 0x65, 0x12, 0x37, 0x0a, 0x0a, 0x61, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74,
+	0x65, 0x73, 0x18, 0x05, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x17, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c,
+	0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x53, 0x74, 0x72, 0x75, 0x63,
+	0x74, 0x52, 0x0a, 0x61, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65, 0x73, 0x42, 0x3b, 0x5a,
+	0x39, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x68, 0x65, 0x66,
+	0x2f, 0x61, 0x75, 0x74, 0x6f, 0x6d, 0x61, 0x74, 0x65, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x65, 0x78,
+	0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x69, 0x6e, 0x66, 0x72, 0x61, 0x5f, 0x70, 0x72, 0x6f,
+	0x78, 0x79, 0x2f, 0x72, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
@@ -521,25 +616,27 @@ func file_external_infra_proxy_request_nodes_proto_rawDescGZIP() []byte {
 	return file_external_infra_proxy_request_nodes_proto_rawDescData
 }
 
-var file_external_infra_proxy_request_nodes_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
+var file_external_infra_proxy_request_nodes_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
 var file_external_infra_proxy_request_nodes_proto_goTypes = []interface{}{
-	(*Nodes)(nil),          // 0: chef.automate.api.infra_proxy.request.Nodes
-	(*Node)(nil),           // 1: chef.automate.api.infra_proxy.request.Node
-	(*AffectedNodes)(nil),  // 2: chef.automate.api.infra_proxy.request.AffectedNodes
-	(*DeleteNode)(nil),     // 3: chef.automate.api.infra_proxy.request.DeleteNode
-	(*UpdateNode)(nil),     // 4: chef.automate.api.infra_proxy.request.UpdateNode
-	(*_struct.Struct)(nil), // 5: google.protobuf.Struct
+	(*Nodes)(nil),                // 0: chef.automate.api.infra_proxy.request.Nodes
+	(*Node)(nil),                 // 1: chef.automate.api.infra_proxy.request.Node
+	(*AffectedNodes)(nil),        // 2: chef.automate.api.infra_proxy.request.AffectedNodes
+	(*DeleteNode)(nil),           // 3: chef.automate.api.infra_proxy.request.DeleteNode
+	(*UpdateNode)(nil),           // 4: chef.automate.api.infra_proxy.request.UpdateNode
+	(*UpdateNodeAttributes)(nil), // 5: chef.automate.api.infra_proxy.request.UpdateNodeAttributes
+	(*_struct.Struct)(nil),       // 6: google.protobuf.Struct
 }
 var file_external_infra_proxy_request_nodes_proto_depIdxs = []int32{
-	5, // 0: chef.automate.api.infra_proxy.request.UpdateNode.automatic_attributes:type_name -> google.protobuf.Struct
-	5, // 1: chef.automate.api.infra_proxy.request.UpdateNode.default_attributes:type_name -> google.protobuf.Struct
-	5, // 2: chef.automate.api.infra_proxy.request.UpdateNode.normal_attributes:type_name -> google.protobuf.Struct
-	5, // 3: chef.automate.api.infra_proxy.request.UpdateNode.override_attributes:type_name -> google.protobuf.Struct
-	4, // [4:4] is the sub-list for method output_type
-	4, // [4:4] is the sub-list for method input_type
-	4, // [4:4] is the sub-list for extension type_name
-	4, // [4:4] is the sub-list for extension extendee
-	0, // [0:4] is the sub-list for field type_name
+	6, // 0: chef.automate.api.infra_proxy.request.UpdateNode.automatic_attributes:type_name -> google.protobuf.Struct
+	6, // 1: chef.automate.api.infra_proxy.request.UpdateNode.default_attributes:type_name -> google.protobuf.Struct
+	6, // 2: chef.automate.api.infra_proxy.request.UpdateNode.normal_attributes:type_name -> google.protobuf.Struct
+	6, // 3: chef.automate.api.infra_proxy.request.UpdateNode.override_attributes:type_name -> google.protobuf.Struct
+	6, // 4: chef.automate.api.infra_proxy.request.UpdateNodeAttributes.attributes:type_name -> google.protobuf.Struct
+	5, // [5:5] is the sub-list for method output_type
+	5, // [5:5] is the sub-list for method input_type
+	5, // [5:5] is the sub-list for extension type_name
+	5, // [5:5] is the sub-list for extension extendee
+	0, // [0:5] is the sub-list for field type_name
 }
 
 func init() { file_external_infra_proxy_request_nodes_proto_init() }
@@ -608,6 +705,18 @@ func file_external_infra_proxy_request_nodes_proto_init() {
 				return nil
 			}
 		}
+		file_external_infra_proxy_request_nodes_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*UpdateNodeAttributes); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -615,7 +724,7 @@ func file_external_infra_proxy_request_nodes_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_external_infra_proxy_request_nodes_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   5,
+			NumMessages:   6,
 			NumExtensions: 0,
 			NumServices:   0,
 		},

api/external/infra_proxy/response/nodes.pb.go
@@ -475,6 +475,72 @@ func (x *Node) GetOverrideAttributes() string {
 	return ""
 }
 
+type UpdateNodeAttributes struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Node name.
+	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	// Node attribute type.
+	Type string `protobuf:"bytes,2,opt,name=type,proto3" json:"type,omitempty"`
+	// Node attributes JSON.
+	Attributes string `protobuf:"bytes,3,opt,name=attributes,proto3" json:"attributes,omitempty"`
+}
+
+func (x *UpdateNodeAttributes) Reset() {
+	*x = UpdateNodeAttributes{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_external_infra_proxy_response_nodes_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *UpdateNodeAttributes) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UpdateNodeAttributes) ProtoMessage() {}
+
+func (x *UpdateNodeAttributes) ProtoReflect() protoreflect.Message {
+	mi := &file_external_infra_proxy_response_nodes_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UpdateNodeAttributes.ProtoReflect.Descriptor instead.
+func (*UpdateNodeAttributes) Descriptor() ([]byte, []int) {
+	return file_external_infra_proxy_response_nodes_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *UpdateNodeAttributes) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+func (x *UpdateNodeAttributes) GetType() string {
+	if x != nil {
+		return x.Type
+	}
+	return ""
+}
+
+func (x *UpdateNodeAttributes) GetAttributes() string {
+	if x != nil {
+		return x.Attributes
+	}
+	return ""
+}
+
 var File_external_infra_proxy_response_nodes_proto protoreflect.FileDescriptor
 
 var file_external_infra_proxy_response_nodes_proto_rawDesc = []byte{
@@ -539,6 +605,12 @@ var file_external_infra_proxy_response_nodes_proto_rawDesc = []byte{
 	0x65, 0x73, 0x12, 0x2f, 0x0a, 0x13, 0x6f, 0x76, 0x65, 0x72, 0x72, 0x69, 0x64, 0x65, 0x5f, 0x61,
 	0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65, 0x73, 0x18, 0x0b, 0x20, 0x01, 0x28, 0x09, 0x52,
 	0x12, 0x6f, 0x76, 0x65, 0x72, 0x72, 0x69, 0x64, 0x65, 0x41, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75,
+	0x74, 0x65, 0x73, 0x22, 0x5e, 0x0a, 0x14, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x4e, 0x6f, 0x64,
+	0x65, 0x41, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65, 0x73, 0x12, 0x12, 0x0a, 0x04, 0x6e,
+	0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12,
+	0x12, 0x0a, 0x04, 0x74, 0x79, 0x70, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x74,
+	0x79, 0x70, 0x65, 0x12, 0x1e, 0x0a, 0x0a, 0x61, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65,
+	0x73, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0a, 0x61, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75,
 	0x74, 0x65, 0x73, 0x42, 0x3c, 0x5a, 0x3a, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f,
 	0x6d, 0x2f, 0x63, 0x68, 0x65, 0x66, 0x2f, 0x61, 0x75, 0x74, 0x6f, 0x6d, 0x61, 0x74, 0x65, 0x2f,
 	0x61, 0x70, 0x69, 0x2f, 0x65, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x69, 0x6e, 0x66,
@@ -558,14 +630,15 @@ func file_external_infra_proxy_response_nodes_proto_rawDescGZIP() []byte {
 	return file_external_infra_proxy_response_nodes_proto_rawDescData
 }
 
-var file_external_infra_proxy_response_nodes_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
+var file_external_infra_proxy_response_nodes_proto_msgTypes = make([]protoimpl.MessageInfo, 7)
 var file_external_infra_proxy_response_nodes_proto_goTypes = []interface{}{
-	(*Nodes)(nil),         // 0: chef.automate.api.infra_proxy.response.Nodes
-	(*AffectedNodes)(nil), // 1: chef.automate.api.infra_proxy.response.AffectedNodes
-	(*NodeAttribute)(nil), // 2: chef.automate.api.infra_proxy.response.NodeAttribute
-	(*DeleteNode)(nil),    // 3: chef.automate.api.infra_proxy.response.DeleteNode
-	(*UpdateNode)(nil),    // 4: chef.automate.api.infra_proxy.response.UpdateNode
-	(*Node)(nil),          // 5: chef.automate.api.infra_proxy.response.Node
+	(*Nodes)(nil),                // 0: chef.automate.api.infra_proxy.response.Nodes
+	(*AffectedNodes)(nil),        // 1: chef.automate.api.infra_proxy.response.AffectedNodes
+	(*NodeAttribute)(nil),        // 2: chef.automate.api.infra_proxy.response.NodeAttribute
+	(*DeleteNode)(nil),           // 3: chef.automate.api.infra_proxy.response.DeleteNode
+	(*UpdateNode)(nil),           // 4: chef.automate.api.infra_proxy.response.UpdateNode
+	(*Node)(nil),                 // 5: chef.automate.api.infra_proxy.response.Node
+	(*UpdateNodeAttributes)(nil), // 6: chef.automate.api.infra_proxy.response.UpdateNodeAttributes
 }
 var file_external_infra_proxy_response_nodes_proto_depIdxs = []int32{
 	2, // 0: chef.automate.api.infra_proxy.response.Nodes.nodes:type_name -> chef.automate.api.infra_proxy.response.NodeAttribute
@@ -655,6 +728,18 @@ func file_external_infra_proxy_response_nodes_proto_init() {
 				return nil
 			}
 		}
+		file_external_infra_proxy_response_nodes_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*UpdateNodeAttributes); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -662,7 +747,7 @@ func file_external_infra_proxy_response_nodes_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_external_infra_proxy_response_nodes_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   6,
+			NumMessages:   7,
 			NumExtensions: 0,
 			NumServices:   0,
 		},

luis33338/automate

api/external/infra_proxy/request/nodes.pb.go
@@ -455,6 +455,81 @@ func (x *UpdateNodeTags) GetTags() []string {
 	return nil
 }
 
+type UpdateNodeAttributes struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Chef organization ID.
+	OrgId string `protobuf:"bytes,1,opt,name=org_id,json=orgId,proto3" json:"org_id,omitempty"`
+	// Chef Infra Server ID.
+	ServerId string `protobuf:"bytes,2,opt,name=server_id,json=serverId,proto3" json:"server_id,omitempty"`
+	// Node name.
+	Name string `protobuf:"bytes,3,opt,name=name,proto3" json:"name,omitempty"`
+	// Node attributes JSON.
+	Attributes *_struct.Struct `protobuf:"bytes,4,opt,name=attributes,proto3" json:"attributes,omitempty"`
+}
+
+func (x *UpdateNodeAttributes) Reset() {
+	*x = UpdateNodeAttributes{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_external_infra_proxy_request_nodes_proto_msgTypes[5]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *UpdateNodeAttributes) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UpdateNodeAttributes) ProtoMessage() {}
+
+func (x *UpdateNodeAttributes) ProtoReflect() protoreflect.Message {
+	mi := &file_external_infra_proxy_request_nodes_proto_msgTypes[5]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UpdateNodeAttributes.ProtoReflect.Descriptor instead.
+func (*UpdateNodeAttributes) Descriptor() ([]byte, []int) {
+	return file_external_infra_proxy_request_nodes_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *UpdateNodeAttributes) GetOrgId() string {
+	if x != nil {
+		return x.OrgId
+	}
+	return ""
+}
+
+func (x *UpdateNodeAttributes) GetServerId() string {
+	if x != nil {
+		return x.ServerId
+	}
+	return ""
+}
+
+func (x *UpdateNodeAttributes) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+func (x *UpdateNodeAttributes) GetAttributes() *_struct.Struct {
+	if x != nil {
+		return x.Attributes
+	}
+	return nil
+}
+
 var File_external_infra_proxy_request_nodes_proto protoreflect.FileDescriptor
 
 var file_external_infra_proxy_request_nodes_proto_rawDesc = []byte{
@@ -523,11 +598,20 @@ var file_external_infra_proxy_request_nodes_proto_rawDesc = []byte{
 	0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x16, 0x0a, 0x06, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x18,
 	0x04, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x12, 0x0a,
 	0x04, 0x74, 0x61, 0x67, 0x73, 0x18, 0x05, 0x20, 0x03, 0x28, 0x09, 0x52, 0x04, 0x74, 0x61, 0x67,
-	0x73, 0x42, 0x3b, 0x5a, 0x39, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f,
-	0x63, 0x68, 0x65, 0x66, 0x2f, 0x61, 0x75, 0x74, 0x6f, 0x6d, 0x61, 0x74, 0x65, 0x2f, 0x61, 0x70,
-	0x69, 0x2f, 0x65, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x69, 0x6e, 0x66, 0x72, 0x61,
-	0x5f, 0x70, 0x72, 0x6f, 0x78, 0x79, 0x2f, 0x72, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x62, 0x06,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x73, 0x22, 0x97, 0x01, 0x0a, 0x14, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x4e, 0x6f, 0x64, 0x65,
+	0x41, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65, 0x73, 0x12, 0x15, 0x0a, 0x06, 0x6f, 0x72,
+	0x67, 0x5f, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x6f, 0x72, 0x67, 0x49,
+	0x64, 0x12, 0x1b, 0x0a, 0x09, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x5f, 0x69, 0x64, 0x18, 0x02,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x49, 0x64, 0x12, 0x12,
+	0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61,
+	0x6d, 0x65, 0x12, 0x37, 0x0a, 0x0a, 0x61, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65, 0x73,
+	0x18, 0x04, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x17, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x53, 0x74, 0x72, 0x75, 0x63, 0x74, 0x52,
+	0x0a, 0x61, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65, 0x73, 0x42, 0x3b, 0x5a, 0x39, 0x67,
+	0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x68, 0x65, 0x66, 0x2f, 0x61,
+	0x75, 0x74, 0x6f, 0x6d, 0x61, 0x74, 0x65, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x65, 0x78, 0x74, 0x65,
+	0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x69, 0x6e, 0x66, 0x72, 0x61, 0x5f, 0x70, 0x72, 0x6f, 0x78, 0x79,
+	0x2f, 0x72, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -542,25 +626,27 @@ func file_external_infra_proxy_request_nodes_proto_rawDescGZIP() []byte {
 	return file_external_infra_proxy_request_nodes_proto_rawDescData
 }
 
-var file_external_infra_proxy_request_nodes_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
+var file_external_infra_proxy_request_nodes_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
 var file_external_infra_proxy_request_nodes_proto_goTypes = []interface{}{
-	(*Nodes)(nil),          // 0: chef.automate.api.infra_proxy.request.Nodes
-	(*Node)(nil),           // 1: chef.automate.api.infra_proxy.request.Node
-	(*AffectedNodes)(nil),  // 2: chef.automate.api.infra_proxy.request.AffectedNodes
-	(*NodeDetails)(nil),    // 3: chef.automate.api.infra_proxy.request.NodeDetails
-	(*UpdateNodeTags)(nil), // 4: chef.automate.api.infra_proxy.request.UpdateNodeTags
-	(*_struct.Struct)(nil), // 5: google.protobuf.Struct
+	(*Nodes)(nil),                // 0: chef.automate.api.infra_proxy.request.Nodes
+	(*Node)(nil),                 // 1: chef.automate.api.infra_proxy.request.Node
+	(*AffectedNodes)(nil),        // 2: chef.automate.api.infra_proxy.request.AffectedNodes
+	(*NodeDetails)(nil),          // 3: chef.automate.api.infra_proxy.request.NodeDetails
+	(*UpdateNodeTags)(nil),       // 4: chef.automate.api.infra_proxy.request.UpdateNodeTags
+	(*UpdateNodeAttributes)(nil), // 5: chef.automate.api.infra_proxy.request.UpdateNodeAttributes
+	(*_struct.Struct)(nil),       // 6: google.protobuf.Struct
 }
 var file_external_infra_proxy_request_nodes_proto_depIdxs = []int32{
-	5, // 0: chef.automate.api.infra_proxy.request.NodeDetails.automatic_attributes:type_name -> google.protobuf.Struct
-	5, // 1: chef.automate.api.infra_proxy.request.NodeDetails.default_attributes:type_name -> google.protobuf.Struct
-	5, // 2: chef.automate.api.infra_proxy.request.NodeDetails.normal_attributes:type_name -> google.protobuf.Struct
-	5, // 3: chef.automate.api.infra_proxy.request.NodeDetails.override_attributes:type_name -> google.protobuf.Struct
-	4, // [4:4] is the sub-list for method output_type
-	4, // [4:4] is the sub-list for method input_type
-	4, // [4:4] is the sub-list for extension type_name
-	4, // [4:4] is the sub-list for extension extendee
-	0, // [0:4] is the sub-list for field type_name
+	6, // 0: chef.automate.api.infra_proxy.request.NodeDetails.automatic_attributes:type_name -> google.protobuf.Struct
+	6, // 1: chef.automate.api.infra_proxy.request.NodeDetails.default_attributes:type_name -> google.protobuf.Struct
+	6, // 2: chef.automate.api.infra_proxy.request.NodeDetails.normal_attributes:type_name -> google.protobuf.Struct
+	6, // 3: chef.automate.api.infra_proxy.request.NodeDetails.override_attributes:type_name -> google.protobuf.Struct
+	6, // 4: chef.automate.api.infra_proxy.request.UpdateNodeAttributes.attributes:type_name -> google.protobuf.Struct
+	5, // [5:5] is the sub-list for method output_type
+	5, // [5:5] is the sub-list for method input_type
+	5, // [5:5] is the sub-list for extension type_name
+	5, // [5:5] is the sub-list for extension extendee
+	0, // [0:5] is the sub-list for field type_name
 }
 
 func init() { file_external_infra_proxy_request_nodes_proto_init() }
@@ -629,6 +715,18 @@ func file_external_infra_proxy_request_nodes_proto_init() {
 				return nil
 			}
 		}
+		file_external_infra_proxy_request_nodes_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*UpdateNodeAttributes); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -636,7 +734,7 @@ func file_external_infra_proxy_request_nodes_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_external_infra_proxy_request_nodes_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   5,
+			NumMessages:   6,
 			NumExtensions: 0,
 			NumServices:   0,
 		},

api/external/infra_proxy/response/nodes.pb.go
@@ -475,6 +475,63 @@ func (x *UpdateNodeTags) GetTags() []string {
 	return nil
 }
 
+type UpdateNodeAttributes struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Node name.
+	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	// Node attributes JSON.
+	Attributes string `protobuf:"bytes,2,opt,name=attributes,proto3" json:"attributes,omitempty"`
+}
+
+func (x *UpdateNodeAttributes) Reset() {
+	*x = UpdateNodeAttributes{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_external_infra_proxy_response_nodes_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *UpdateNodeAttributes) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UpdateNodeAttributes) ProtoMessage() {}
+
+func (x *UpdateNodeAttributes) ProtoReflect() protoreflect.Message {
+	mi := &file_external_infra_proxy_response_nodes_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UpdateNodeAttributes.ProtoReflect.Descriptor instead.
+func (*UpdateNodeAttributes) Descriptor() ([]byte, []int) {
+	return file_external_infra_proxy_response_nodes_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *UpdateNodeAttributes) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+func (x *UpdateNodeAttributes) GetAttributes() string {
+	if x != nil {
+		return x.Attributes
+	}
+	return ""
+}
+
 var File_external_infra_proxy_response_nodes_proto protoreflect.FileDescriptor
 
 var file_external_infra_proxy_response_nodes_proto_rawDesc = []byte{
@@ -539,11 +596,16 @@ var file_external_infra_proxy_response_nodes_proto_rawDesc = []byte{
 	0x76, 0x65, 0x72, 0x72, 0x69, 0x64, 0x65, 0x41, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65,
 	0x73, 0x22, 0x24, 0x0a, 0x0e, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x4e, 0x6f, 0x64, 0x65, 0x54,
 	0x61, 0x67, 0x73, 0x12, 0x12, 0x0a, 0x04, 0x74, 0x61, 0x67, 0x73, 0x18, 0x05, 0x20, 0x03, 0x28,
-	0x09, 0x52, 0x04, 0x74, 0x61, 0x67, 0x73, 0x42, 0x3c, 0x5a, 0x3a, 0x67, 0x69, 0x74, 0x68, 0x75,
-	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x68, 0x65, 0x66, 0x2f, 0x61, 0x75, 0x74, 0x6f, 0x6d,
-	0x61, 0x74, 0x65, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x65, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c,
-	0x2f, 0x69, 0x6e, 0x66, 0x72, 0x61, 0x5f, 0x70, 0x72, 0x6f, 0x78, 0x79, 0x2f, 0x72, 0x65, 0x73,
-	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x09, 0x52, 0x04, 0x74, 0x61, 0x67, 0x73, 0x22, 0x4a, 0x0a, 0x14, 0x55, 0x70, 0x64, 0x61, 0x74,
+	0x65, 0x4e, 0x6f, 0x64, 0x65, 0x41, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65, 0x73, 0x12,
+	0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e,
+	0x61, 0x6d, 0x65, 0x12, 0x1e, 0x0a, 0x0a, 0x61, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75, 0x74, 0x65,
+	0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0a, 0x61, 0x74, 0x74, 0x72, 0x69, 0x62, 0x75,
+	0x74, 0x65, 0x73, 0x42, 0x3c, 0x5a, 0x3a, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f,
+	0x6d, 0x2f, 0x63, 0x68, 0x65, 0x66, 0x2f, 0x61, 0x75, 0x74, 0x6f, 0x6d, 0x61, 0x74, 0x65, 0x2f,
+	0x61, 0x70, 0x69, 0x2f, 0x65, 0x78, 0x74, 0x65, 0x72, 0x6e, 0x61, 0x6c, 0x2f, 0x69, 0x6e, 0x66,
+	0x72, 0x61, 0x5f, 0x70, 0x72, 0x6f, 0x78, 0x79, 0x2f, 0x72, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73,
+	0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -558,14 +620,15 @@ func file_external_infra_proxy_response_nodes_proto_rawDescGZIP() []byte {
 	return file_external_infra_proxy_response_nodes_proto_rawDescData
 }
 
-var file_external_infra_proxy_response_nodes_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
+var file_external_infra_proxy_response_nodes_proto_msgTypes = make([]protoimpl.MessageInfo, 7)
 var file_external_infra_proxy_response_nodes_proto_goTypes = []interface{}{
-	(*Nodes)(nil),          // 0: chef.automate.api.infra_proxy.response.Nodes
-	(*AffectedNodes)(nil),  // 1: chef.automate.api.infra_proxy.response.AffectedNodes
-	(*NodeAttribute)(nil),  // 2: chef.automate.api.infra_proxy.response.NodeAttribute
-	(*DeleteNode)(nil),     // 3: chef.automate.api.infra_proxy.response.DeleteNode
-	(*Node)(nil),           // 4: chef.automate.api.infra_proxy.response.Node
-	(*UpdateNodeTags)(nil), // 5: chef.automate.api.infra_proxy.response.UpdateNodeTags
+	(*Nodes)(nil),                // 0: chef.automate.api.infra_proxy.response.Nodes
+	(*AffectedNodes)(nil),        // 1: chef.automate.api.infra_proxy.response.AffectedNodes
+	(*NodeAttribute)(nil),        // 2: chef.automate.api.infra_proxy.response.NodeAttribute
+	(*DeleteNode)(nil),           // 3: chef.automate.api.infra_proxy.response.DeleteNode
+	(*Node)(nil),                 // 4: chef.automate.api.infra_proxy.response.Node
+	(*UpdateNodeTags)(nil),       // 5: chef.automate.api.infra_proxy.response.UpdateNodeTags
+	(*UpdateNodeAttributes)(nil), // 6: chef.automate.api.infra_proxy.response.UpdateNodeAttributes
 }
 var file_external_infra_proxy_response_nodes_proto_depIdxs = []int32{
 	2, // 0: chef.automate.api.infra_proxy.response.Nodes.nodes:type_name -> chef.automate.api.infra_proxy.response.NodeAttribute
@@ -655,6 +718,18 @@ func file_external_infra_proxy_response_nodes_proto_init() {
 				return nil
 			}
 		}
+		file_external_infra_proxy_response_nodes_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*UpdateNodeAttributes); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -662,7 +737,7 @@ func file_external_infra_proxy_response_nodes_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_external_infra_proxy_response_nodes_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   6,
+			NumMessages:   7,
 			NumExtensions: 0,
 			NumServices:   0,
 		},